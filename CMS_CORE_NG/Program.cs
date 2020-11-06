ebe6f2b2b55f2708bbf1fe3fff6d7dbfd601417c
MaevaL <maeva.lauzier@gmail.com>
2020-11-06 15:56:14 +0100

    ASP.NET CORE and Angular base project with LoggingService

---
+++ CMS_CORE_NG/Program.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using LoggingService;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace CMS_CORE_NG
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scopte = host.Services.CreateScope())
+            {
+                try
+                {
+                    int z = 0;
+                    int result = 1 / z;
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Log.Error("An error occured while seeding the database {Error} {StackTrace} {InnerException} {Source}",
+                        ex.Message, ex.StackTrace, ex.InnerException, ex.Source);
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                      .Enrich.FromLogContext()
+                      .Enrich.WithProperty("Application", "CMS_CORE_NG")
+                      .Enrich.WithProperty("MachineName", Environment.MachineName)
+                      .Enrich.WithProperty("CurrentManagedThreadId", Environment.CurrentManagedThreadId)
+                      .Enrich.WithProperty("OSVersion", Environment.OSVersion)
+                      .Enrich.WithProperty("Version", Environment.Version)
+                      .Enrich.WithProperty("UserName", Environment.UserName)
+                      .Enrich.WithProperty("ProcessId", Process.GetCurrentProcess().Id)
+                      .Enrich.WithProperty("ProcessName", Process.GetCurrentProcess().ProcessName)
+                      .WriteTo.File(formatter: new CustomTextFormatter(),
+                      path: Path.Combine(hostingContext.HostingEnvironment.ContentRootPath +
+                      $"{Path.DirectorySeparatorChar}Logs{Path.DirectorySeparatorChar}",
+                      $"load_error_{ DateTime.Now:yyyyMMdd}.txt"))
+                      .ReadFrom.Configuration(hostingContext.Configuration));
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
